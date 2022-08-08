e121ba137e1cc9cd25c3e79f06972458e6f219dd
oxterisk <oxterisk@protonmail.com>
2022-08-08 20:18:13 +0200

    initial commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create( ['name' => 'Admin', 'email' => 'admin@example.com', 'password' => bcrypt('12345678'), 'is_admin' => 1] );
+    }
+}
